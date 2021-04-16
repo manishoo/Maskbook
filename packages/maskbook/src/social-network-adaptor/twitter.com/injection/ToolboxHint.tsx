df28edad4090e0f1cacaff816faecb383d2f7655
lelenei <zwpsky@gmail.com>
2021-04-16 10:13:38 +0800

    fix: change color from background color

--- packages/maskbook/src/social-network-adaptor/twitter.com/injection/ToolboxHint.tsx
+++ packages/maskbook/src/social-network-adaptor/twitter.com/injection/ToolboxHint.tsx
@@ -3,7 +3,8 @@ import { ToolboxHint } from '../../../components/InjectedComponents/ToolboxHint'
 import { createReactRootShadowed } from '../../../utils/shadow-root/renderInShadowRoot'
 import { toolBoxInSideBarSelector } from '../utils/selector'
 import { startWatch } from '../../../utils/watcher'
-import { createStyles, makeStyles } from '@material-ui/core'
+import { createStyles, makeStyles, Theme } from '@material-ui/core'
+import { fromRGB, getBackgroundColor } from '../../../utils/theme-tools'
 
 export function injectToolboxHintAtTwitter(signal: AbortSignal) {
     const watcher = new MutationObserverWatcher(toolBoxInSideBarSelector())
@@ -11,8 +12,63 @@ export function injectToolboxHintAtTwitter(signal: AbortSignal) {
     createReactRootShadowed(watcher.firstDOMProxy.afterShadow, { signal }).render(<ToolboxHintAtTwitter />)
 }
 
+function getColor(backgroundColor: string, theme: Theme): string {
+    const color = fromRGB(backgroundColor)
+    if (!color) {
+        return theme.palette.mode === 'dark' ? 'rgb(217, 217, 217' : 'rgb(15, 20, 25)'
+    }
+    if (color?.[0] === 21 && color?.[1] === 32 && color?.[2] === 43) {
+        return 'rgb(255, 255, 255)'
+    }
+    if (color?.[0] === 0 && color?.[1] === 0 && color?.[2] === 0) {
+        return 'rgb(217, 217, 217)'
+    }
+
+    if (color?.[0] === 255 && color?.[1] === 255 && color?.[2] === 255) {
+        return 'rgb(15, 20, 25)'
+    }
+
+    return theme.palette.mode === 'dark' ? 'rgb(217, 217, 217' : 'rgb(15, 20, 25)'
+}
+
+function getActiveColor(backgroundColor: string, theme: Theme): string {
+    const color = fromRGB(backgroundColor)
+    if (!color) {
+        return theme.palette.mode === 'dark' ? 'rgb(13, 29, 48)' : 'rgb(212,237,252)'
+    }
+    if (color?.[0] === 21 && color?.[1] === 32 && color?.[2] === 43) {
+        return 'rgb(23, 46, 63)'
+    }
+    if (color?.[0] === 0 && color?.[1] === 0 && color?.[2] === 0) {
+        return 'rgb(13, 29, 48)'
+    }
+
+    if (color?.[0] === 255 && color?.[1] === 255 && color?.[2] === 255) {
+        return 'rgb(212,237,252)'
+    }
+
+    return theme.palette.mode === 'dark' ? 'rgb(13, 29, 48)' : 'rgb(212,237,252)'
+}
+
+function getHoverColor(backgroundColor: string, theme: Theme): string {
+    const color = fromRGB(backgroundColor)
+    if (!color) {
+        return theme.palette.mode === 'dark' ? 'rgb(7, 15, 25)' : 'rgb(233, 246, 253)'
+    }
+    if (color?.[0] === 21 && color?.[1] === 32 && color?.[2] === 43) {
+        return 'rgb(23, 46, 63)'
+    }
+    if (color?.[0] === 0 && color?.[1] === 0 && color?.[2] === 0) {
+        return 'rgb(7, 15, 25)'
+    }
+    if (color?.[0] === 255 && color?.[1] === 255 && color?.[2] === 255) {
+        return 'rgb(233, 246, 253)'
+    }
+    return theme.palette.mode === 'dark' ? 'rgb(7, 15, 25)' : 'rgb(233, 246, 253)'
+}
+
 const useStyles = makeStyles((theme) =>
-    createStyles<string, { color?: string }>({
+    createStyles<string, { backgroundColor: string }>({
         wrapper: {
             paddingTop: 4,
             paddingBottom: 4,
@@ -26,39 +82,32 @@ const useStyles = makeStyles((theme) =>
             paddingBottom: theme.spacing(2),
         },
         title: {
-            color: (props) => (props.color ? props.color : 'rgb(217, 217, 217)'),
+            color: (props) => getColor(props.backgroundColor, theme),
         },
         text: {
             marginLeft: 12,
             fontSize: 15,
-            color: theme.palette.mode === 'dark' ? 'rgb(216, 216, 216)' : 'rgb(15, 20, 25)',
+            color: theme.palette.mode === 'dark' ? 'rgb(217, 217, 217)' : 'rgb(15, 20, 25)',
             paddingRight: theme.spacing(2),
         },
         icon: {
-            color: (props) => (props.color ? props.color : 'rgb(217, 217, 217)'),
+            color: (props) => getColor(props.backgroundColor, theme),
         },
         button: {
             '&:hover': {
-                backgroundColor: theme.palette.mode === 'dark' ? 'rgb(7, 15, 25)' : 'rgb(233, 246, 253)',
+                backgroundColor: (props) => getHoverColor(props.backgroundColor, theme),
             },
             '&:active': {
-                backgroundColor: theme.palette.mode === 'dark' ? 'rgb(13, 29, 48)' : 'rgb(212,237,252)',
+                backgroundColor: (props) => getActiveColor(props.backgroundColor, theme),
             },
         },
     }),
 )
 
-function GetColor() {
-    const ele = new LiveSelector()
-        .querySelector<HTMLDivElement>('[data-testid="AppTabBar_More_Menu"]')
-        .querySelector<HTMLDivElement>('div')
-        .querySelectorAll<HTMLDivElement>('div')
-    const style = window.getComputedStyle(ele.evaluate()[1], null)
-    return style.color
-}
 function ToolboxHintAtTwitter() {
-    const color = GetColor()
-    const classes = useStyles({ color })
+    const ele = new LiveSelector().querySelector<HTMLDivElement>('body')
+    const backgroundColor = getBackgroundColor(ele.evaluate()[0])
+    const classes = useStyles({ backgroundColor })
 
     // Todo: add click handler
     return (
